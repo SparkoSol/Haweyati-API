451f8334be1e874cb15ab54b17b45b15d0bfe36f
ZainUrRehmanKhan <zainkhan.official10@gmail.com>
2020-10-31 15:18:05 +0500

    * Customer fixed. * Testing Web hooks

--- Haweyati-API-master/src/modules/customers/customers.controller.ts
+++ Haweyati-API-master/src/modules/customers/customers.controller.ts
@@ -19,38 +19,6 @@ export class CustomersController extends ImageController<ICustomerInterface> {
     super(service)
   }
 
-  @Get('getactive')
-  async getActive(): Promise<any> {
-    return await this.service.fetch()
-  }
-
-  @Get('getactive/:id')
-  async getActiveById(@Param('id') id: string): Promise<any> {
-    return await this.service.fetch(id)
-  }
-
-  @Get('getblocked')
-  async getAllBlocked(): Promise<any> {
-    return await this.service.getBlocked()
-  }
-
-  @Get('getall')
-  async getAll(): Promise<ICustomerInterface[] | ICustomerInterface>{
-    return await this.service.getAll();
-  }
-
-  @Get('getall/:id')
-  async getById(@Param('id') id: string): Promise<ICustomerInterface[] | ICustomerInterface>{
-    return await this.service.getAll(id);
-  }
-
-  @Get('getprofile/:contact')
-  async getProfile(
-    @Param('contact') contact: string
-  ): Promise<ICustomerInterface | string> {
-    return await this.service.getProfile(contact)
-  }
-
   @Patch('getblocked/:id')
   async getBlocked(
     @Param('id') id: string,
@@ -60,9 +28,9 @@ export class CustomersController extends ImageController<ICustomerInterface> {
     else return await this.service.getBlocked(id)
   }
 
-  @Patch('getunblocked/:id')
-  async getUnblocked(@Param('id') id: string): Promise<any> {
-    return await this.service.getUnblocked(id)
+  @Get('getactive')
+  async getActive(): Promise<any> {
+    return await this.service.fetch()
   }
 
   @Get('active-search')
@@ -70,8 +38,40 @@ export class CustomersController extends ImageController<ICustomerInterface> {
     return await this.service.searchActive(query)
   }
 
+  @Get('getblocked')
+  async getAllBlocked(): Promise<any> {
+    return await this.service.getBlocked()
+  }
+
+  @Get('getprofile/:contact')
+  async getProfile(
+    @Param('contact') contact: string
+  ): Promise<ICustomerInterface | string> {
+    return await this.service.getProfile(contact)
+  }
+
   @Get('blocked-search')
   async searchBlocked(@Query() query: any){
     return await this.service.searchBlocked(query)
+  }
+
+  @Patch('getunblocked/:id')
+  async getUnblocked(@Param('id') id: string): Promise<any> {
+    return await this.service.getUnblocked(id)
+  }
+
+  @Get('getall')
+  async getAll(): Promise<ICustomerInterface[] | ICustomerInterface>{
+    return await this.service.getAll();
+  }
+
+  @Get('getactive/:id')
+  async getActiveById(@Param('id') id: string): Promise<any> {
+    return await this.service.fetch(id)
+  }
+
+  @Get('getall/:id')
+  async getById(@Param('id') id: string): Promise<ICustomerInterface[] | ICustomerInterface>{
+    return await this.service.getAll(id);
   }
 }
