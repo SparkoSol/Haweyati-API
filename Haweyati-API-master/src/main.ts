ee061030363bafc337f781f9bbc044c24a1f0c5f
ZainUrRehmanKhan <zainkhan.official10@gmail.com>
2021-04-12 15:47:59 +0500

    * main.ts changed

--- Haweyati-API-master/src/main.ts
+++ Haweyati-API-master/src/main.ts
@@ -1,17 +1,17 @@
-import { join } from 'path';
-import { AppModule } from "./app.module";
-import { NestFactory } from "@nestjs/core";
-import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path'
+import { AppModule } from './app.module'
+import { NestFactory } from '@nestjs/core'
+import { NestExpressApplication } from '@nestjs/platform-express'
 
 async function runApp() {
-    const app = await NestFactory.create<NestExpressApplication>(AppModule);
-    app.enableCors();
-    app.useStaticAssets(
-      join(__dirname, '..', '..',  'uploads'),
-       {
-           prefix: '/uploads/'
-       }
-    )
-    await app.listen(4000, '0.0.0.0');
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.enableCors();
+  app.useStaticAssets(
+    join(__dirname, '..', '..', 'uploads'),
+    {
+      prefix: '/uploads/'
+    }
+  )
+  await app.listen(4000, '0.0.0.0');
 }
 runApp();
